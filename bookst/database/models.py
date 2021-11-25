ad68a23e877536dee6fd15bac0a040ad89d234c2
can <laksika.0175705@gmail.com>
2021-11-25 17:54:27 +0700

    d

---
+++ bookst/database/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+
+import datetime
+
+
+class Book(models.Model):
+    book_text = models.CharField(max_length=60)
+    loan_date = models.DateTimeField('Loan Date')
+    url_image = models.CharField(max_length=200)
+    name_user =models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.book_text
+    def was_published_recently(self):
+        return self.loan_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.choice_text
+    
